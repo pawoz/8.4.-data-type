dcfce79affdfc6782cf15fdc6e634598411ec016
pawoz <pawel.wozny@op.pl>
2017-04-01 22:05:50 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+var a = prompt('Podstawa trójkąta');
+var h = prompt('Wysokość trójkąta');
+var triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
